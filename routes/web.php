b3d2bf9fee6a92b1e448d1d7dc303d2fb843f952
Jan Rey Suarez <w33haa2@gmail.com>
2018-09-18 21:09:42 +0800

    test
    test

--- routes/web.php
+++ routes/web.php
@@ -11,4 +11,10 @@ Auth::routes();
 Route::group([ 'middleware'=>['auth']], function () {
     Route::get('/', 'DashboardController@index')->name('dashboard');
     Route::resource('users', 'UserController');
+    Route::get('/email', function () {
+        return view('admin.dashboard.email');
+    });
+    Route::get('/forms', function () {
+        return view('admin.dashboard.forms');
+    });
 });
