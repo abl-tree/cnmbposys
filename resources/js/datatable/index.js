44913ec114e91b4391e47a2610f6e771582a1fbc
Jan Rey Suarez <w33haa2@gmail.com>
2018-09-19 01:36:01 +0800

    maoni
    mao ni

--- resources/js/datatable/index.js
+++ resources/js/datatable/index.js
@@ -2,7 +2,8 @@ import * as $ from 'jquery';
 import 'datatables';
 
 export default (function () {
-  $('#dataTable').DataTable({
+
+  $('#employee').DataTable({
 		language: {
 			// 'url' : 'https://cdn.datatables.net/plug-ins/1.10.16/i18n/French.json'
 			// More languages : http://www.datatables.net/plug-ins/i18n/
