3320f0b9bc54b99e5bfce5d4a284d8c0e73388a9
Floyd Francis Matabilas <floydignified@gmail.com>
2018-12-30 14:09:33 +0800

    Floyd > agent schedule CRUD

---
+++ app/Data/Repositories/AgentScheduleRepository.php
@@ -0,0 +1,205 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Janrey
+ * Date: 30/10/2018
+ * Time: 2:12 PM
+ */
+
+namespace App\Data\Repositories;
+
+use App\Data\Models\AgentSchedule;
+use App\Data\Models\UserInfo;
+use App\Data\Repositories\BaseRepository;
+
+class AgentScheduleRepository extends BaseRepository
+{
+
+    protected $agent_schedule;
+
+    public function __construct(
+        AgentSchedule $agentSchedule
+    ) {
+        $this->agent_schedule = $agentSchedule;
+    }
+
+    public function defineAgentSchedule($data = [])
+    {
+        // data validation
+        if (!isset($data['id'])) {
+
+            if (!isset($data['user_id']) ||
+                !is_numeric($data['user_id']) ||
+                $data['user_id'] <= 0) {
+                return $this->setResponse([
+                    'code'  => 500,
+                    'title' => "User ID is not set.",
+                ]);
+            }
+
+            if (!isset($data['title'])) {
+                return $this->setResponse([
+                    'code'  => 500,
+                    'title' => "Title is not set.",
+                ]);
+            }
+
+            if (!isset($data['start_event'])) {
+                return $this->setResponse([
+                    'code'  => 500,
+                    'title' => "Start date is not set.",
+                ]);
+            }
+
+            if (!isset($data['end_event'])) {
+                return $this->setResponse([
+                    'code'  => 500,
+                    'title' => "End date is not set.",
+                ]);
+            }
+
+        }
+        // data validation
+
+        // existence check
+
+        if (isset($data['user_id'])) {
+            if (!UserInfo::find($data['user_id'])) {
+                return $this->setResponse([
+                    'code'  => 500,
+                    'title' => "User ID is not available.",
+                ]);
+            }
+        }
+
+        if (isset($data['id'])) {
+            $does_exist = $this->agent_schedule->find($data['id']);
+
+            if (!$does_exist) {
+                return $this->setResponse([
+                    'code'  => 500,
+                    'title' => 'Agent Schedule ID does not exist.',
+                ]);
+            }
+        }
+
+        // existence check
+
+        // insertion
+
+        if (isset($data['id'])) {
+            $agent_schedule = $this->agent_schedule->find($data['id']);
+        } else {
+            $agent_schedule = $this->agent_schedule->init($this->agent_schedule->pullFillable($data));
+        }
+
+        if (!$agent_schedule->save($data)) {
+            return $this->setResponse([
+                "code"        => 500,
+                "title"       => "Data Validation Error.",
+                "description" => "An error was detected on one of the inputted data.",
+                "meta"        => [
+                    "errors" => $agent_schedule->errors(),
+                ],
+            ]);
+        }
+
+        return $this->setResponse([
+            "code"       => 200,
+            "title"      => "Successfully defined an agent schedule.",
+            "parameters" => $agent_schedule,
+        ]);
+
+        // insertion
+
+    }
+
+    public function deleteAgentSchedule($data = [])
+    {
+        $record = $this->agent_schedule->find($data['id']);
+
+        if (!$record) {
+            return $this->setResponse([
+                "code"        => 404,
+                "title"       => "Agent schedule not found"
+            ]);
+        }
+
+        if (!$record->delete()) {
+            return $this->setResponse([
+                "code"    => 500,
+                "message" => "Deleting agent schedule was not successful.",
+                "meta"    => [
+                    "errors" => $record->errors(),
+                ],
+                "parameters" => [
+                    'schedule_id' => $data['id']
+                ]
+            ]);
+        }
+
+        return $this->setResponse([
+            "code"        => 200,
+            "title"       => "Agent schedule deleted",
+            "description" => "An agent schedule was deleted.",
+            "parameters"        => [
+                "schedule_id" => $data['id']
+            ]
+        ]);
+
+    }
+
+    public function fetchAgentSchedule($data = [])
+    {
+        $meta_index = "agent_schedules";
+        $parameters = [];
+        $count      = 0;
+
+        if (isset($data['id']) &&
+            is_numeric($data['id'])) {
+
+            $meta_index     = "agent_schedule";
+            $data['single'] = true;
+            $data['where']  = [
+                [
+                    "target"   => "id",
+                    "operator" => "=",
+                    "value"    => $data['id'],
+                ],
+            ];
+
+            $parameters['agent_schedule_id'] = $data['id'];
+
+        }
+
+        $count_data = $data;
+
+        $data['relations'] = "user_info";
+
+        $result     = $this->fetchGeneric($data, $this->agent_schedule);
+
+        if (!$result) {
+            return $this->setResponse([
+                'code'       => 404,
+                'title'      => "No agent schedules are found",
+                "meta"       => [
+                    $meta_index => $result,
+                ],
+                "parameters" => $parameters,
+            ]);
+        }
+
+        $count = $this->countData($count_data, refresh_model($this->agent_schedule->getModel()));
+
+        return $this->setResponse([
+            "code"       => 200,
+            "title"      => "Successfully retrieved agent schedules",
+            "meta"       => [
+                $meta_index => $result,
+                "count"     => $count,
+            ],
+            "parameters" => $parameters,
+        ]);
+    }
+
+}
---
+++ app/Http/Controllers/AgentScheduleController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\BaseController;
+use App\Data\Repositories\AgentScheduleRepository;
+
+class AgentScheduleController extends BaseController
+{
+    protected $agent_schedule_repo;
+
+    public function __construct(
+        AgentScheduleRepository $agentScheduleRepository
+    ){
+        $this->agent_schedule_repo = $agentScheduleRepository;
+    }
+
+    public function all(Request $request)
+    {
+        $data = $request->all();
+        return $this->absorb($this->agent_schedule_repo->fetchAgentSchedule($data))->json();
+    }
+
+    public function create(Request $request)
+    {
+        $data = $request->all();
+        return $this->absorb($this->agent_schedule_repo->defineAgentSchedule($data))->json();
+    }
+
+    public function delete(Request $request, $id)
+    {
+        $data       = $request->all();
+        $data['id'] = $id;
+
+        if (!isset($data['id']) ||
+            !is_numeric($data['id']) ||
+            $data['id'] <= 0) {
+            return $this->setResponse([
+                'code'  => 500,
+                'title' => "Schedule ID is not set.",
+            ]);
+        }
+
+        return $this->absorb($this->agent_schedule_repo->deleteAgentSchedule($data))->json();
+    }
+
+
+    public function fetch(Request $request, $id)
+    {
+        $data = $request->all();
+        $data['id'] = $id;
+
+        if (!isset($data['id']) ||
+            !is_numeric($data['id']) ||
+            $data['id'] <= 0) {
+            return $this->setResponse([
+                'code'  => 500,
+                'title' => "Schedule ID is invalid.",
+            ]);
+        }
+
+        return $this->absorb($this->agent_schedule_repo->fetchAgentSchedule($data))->json();
+    }
+
+    public function update(Request $request, $id)
+    {
+        $data = $request->all();
+        $data['id'] = $id;
+
+        return $this->absorb($this->agent_schedule_repo->defineAgentSchedule($data))->json();
+    }
+
+
+}
