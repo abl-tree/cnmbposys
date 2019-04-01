2091d7bf96065de67b8a342132cc320f30bff334
Floyd Francis Matabilas <floydignified@gmail.com>
2019-04-02 02:24:12 +0800

    Floyd > refactor agent schedule repo

--- app/Data/Repositories/AgentScheduleRepository.php
+++ app/Data/Repositories/AgentScheduleRepository.php
@@ -21,14 +21,20 @@ class AgentScheduleRepository extends BaseRepository
 
     protected 
         $agent_schedule,
-        $user;
+        $user,
+        $user_info,
+        $event_title;
 
     public function __construct(
         AgentSchedule $agentSchedule,
-        User $user
+        User $user,
+        UserInfo $userInfo,
+        EventTitle $eventTitle
     ) {
         $this->agent_schedule = $agentSchedule;
         $this->user = $user;
+        $this->user_info = $userInfo;
+        $this->event_title = $eventTitle;
     }
 
     public function excelData($data)
@@ -122,7 +128,7 @@ class AgentScheduleRepository extends BaseRepository
         // existence check
 
         if (isset($data['user_id'])) {
-            if (!UserInfo::find($data['user_id'])) {
+            if (!$this->user_info->find($data['user_id'])) {
                 return $this->setResponse([
                     'code'  => 500,
                     'title' => "User ID is not available.",
@@ -131,7 +137,7 @@ class AgentScheduleRepository extends BaseRepository
         }
 
         if (isset($data['title_id'])) {
-            if (!EventTitle::find($data['title_id'])) {
+            if (!$this->event_title->find($data['title_id'])) {
                 return $this->setResponse([
                     'code'  => 500,
                     'title' => "Title ID is not available.",
