eb733c5a0a1208dfbd2a6267b2d354ebefc42c82
janjanjanjan <jsuarez@maersummit.tech>
2019-04-07 10:40:59 +0800

    janrey >> excel date time to php

---
+++ app/Providers/ExcelDateConverter.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use App\Services\ExcelDateService;
+
+class ExcelDateConverter extends ServiceProvider
+{
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->bind('App\Library\Services\DemoOne', function ($app) {
+            return new ExcelDateService();
+        });
+    }
+}
--- config/app.php
+++ config/app.php
@@ -161,6 +161,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         Yajra\Datatables\DataTablesServiceProvider::class,
+        App\Providers\ExcelDateConverter::class,
 
 
     ],
