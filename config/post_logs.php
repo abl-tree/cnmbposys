b82a1ad297faa6d1e2bf95190a4d011c8efa0240
Floyd Matabilas <fmatabilas@maersummit.tech>
2019-04-20 20:55:08 +0800

    Floyd > post logs config, with request schedule slugs

---
+++ config/post_logs.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+
+    'request_schedules' => [
+        'create' => [
+            'slug' => 'Successfully created a request schedule for **target_name** [**target_position**] on **start_date** to **end_date**, executed by **logged_name** [**logged_position**]',
+        ],
+        'update' => [
+            'slug' => 'Successfully updated a request schedule for **target_name** [**target_position**], executed by **logged_name** [**logged_position**]',
+        ],
+        'delete' => [
+            'slug' => 'Successfully deleted a request schedule for **target_name** [**target_position**], executed by **logged_name** [**logged_position**]',
+        ],
+    ],
+
+];
