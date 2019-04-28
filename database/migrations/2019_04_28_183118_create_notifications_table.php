8d25c0a9d760fab496e7045dfbede3edf1299318
Floyd Francis Matabilas <floydignified@gmail.com>
2019-04-28 20:44:42 +0800

    Floyd > add notification table migration

---
+++ database/migrations/2019_04_28_183118_create_notifications_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateNotificationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('notifications', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('description');
+            $table->string('type');
+            $table->integer('type_id');
+            $table->integer('sender_id');
+            $table->integer('recipient_id');
+            $table->string('status')->default('unread');
+            $table->dateTime('read_at')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('notifications');
+    }
+}
