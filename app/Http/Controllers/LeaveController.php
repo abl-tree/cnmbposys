cbd5ebbd801c5ddf67209f482e017126895006ad
Floyd Matabilas <fmatabilas@maersummit.tech>
2019-07-22 00:07:38 +0800

    Floyd > leave and leave credit controllers

---
+++ app/Http/Controllers/LeaveController.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Data\Repositories\LeaveRepository;
+use App\Http\Controllers\BaseController;
+use Illuminate\Http\Request;
+
+class LeaveController extends BaseController
+{
+    protected $leave_repo;
+
+    public function __construct(
+        LeaveRepository $leaveRepository
+    ) {
+        $this->leave_repo = $leaveRepository;
+    }
+
+    public function all(Request $request)
+    {
+        $data = $request->all();
+        $data['user_access'] = $request->user()->access->id;
+        return $this->absorb($this->leave_repo->fetchLeave($data))->json();
+    }
+
+    public function approval(Request $request, $action, $id)
+    {
+        $data = $request->all();
+        $data['id'] = $id;
+        $data['status'] = $action == "approve" ? 'approved' : 'rejected';
+        $data['user_access'] = $request->user()->access->id;
+
+        if (!isset($data['id']) ||
+            !is_numeric($data['id']) ||
+            $data['id'] <= 0) {
+            return $this->setResponse([
+                'code' => 500,
+                'title' => "Leave ID is invalid.",
+            ]);
+        }
+
+        return $this->absorb($this->leave_repo->setLeaveApproval($data))->json();
+    }
+
+    public function create(Request $request)
+    {
+        $data = $request->all();
+        $data['generated_by'] = $request->user()->id;
+        $data['allowed_access'] = $request->user()->access->id;
+        return $this->absorb($this->leave_repo->defineLeave($data))->json();
+    }
+
+    public function delete(Request $request, $id)
+    {
+        $data = $request->all();
+        $data['id'] = $id;
+        $data['user_access'] = $request->user()->access->id;
+
+        if (!isset($data['id']) ||
+            !is_numeric($data['id']) ||
+            $data['id'] <= 0) {
+            return $this->setResponse([
+                'code' => 500,
+                'title' => "Leave ID is not set.",
+            ]);
+        }
+
+        return $this->absorb($this->leave_repo->deleteLeave($data))->json();
+    }
+
+    public function fetch(Request $request, $id)
+    {
+        $data = $request->all();
+        $data['id'] = $id;
+        $data['user_access'] = $request->user()->access->id;
+
+        if (!isset($data['id']) ||
+            !is_numeric($data['id']) ||
+            $data['id'] <= 0) {
+            return $this->setResponse([
+                'code' => 500,
+                'title' => "Leave ID is invalid.",
+            ]);
+        }
+
+        return $this->absorb($this->leave_repo->fetchLeave($data))->json();
+    }
+
+    public function update(Request $request, $id)
+    {
+        $data = $request->all();
+        $data['id'] = $id;
+        $data['user_access'] = $request->user()->access->id;
+
+        return $this->absorb($this->leave_repo->defineLeave($data))->json();
+    }
+
+    public function search(Request $request)
+    {
+        $data = $request->all();
+        $data['user_access'] = $request->user()->access->id;
+        return $this->absorb($this->leave_repo->searchLeave($data))->json();
+    }
+
+}
---
+++ app/Http/Controllers/LeaveCreditController.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Data\Repositories\LeaveCreditRepository;
+use App\Http\Controllers\BaseController;
+use Illuminate\Http\Request;
+
+class LeaveCreditController extends BaseController
+{
+    protected $leave_credit_repo;
+
+    public function __construct(
+        LeaveCreditRepository $leaveCreditRepository
+    ) {
+        $this->leave_credit_repo = $leaveCreditRepository;
+    }
+
+    public function all(Request $request)
+    {
+        $data = $request->all();
+        return $this->absorb($this->leave_credit_repo->fetchLeaveCredit($data))->json();
+    }
+
+    public function create(Request $request)
+    {
+        $data = $request->all();
+        return $this->absorb($this->leave_credit_repo->defineLeaveCredit($data))->json();
+    }
+
+    public function delete(Request $request, $id)
+    {
+        $data = $request->all();
+        $data['id'] = $id;
+
+        if (!isset($data['id']) ||
+            !is_numeric($data['id']) ||
+            $data['id'] <= 0) {
+            return $this->setResponse([
+                'code' => 500,
+                'title' => "Leave credit ID is not set.",
+            ]);
+        }
+
+        return $this->absorb($this->leave_credit_repo->deleteLeaveCredit($data))->json();
+    }
+
+    public function fetch(Request $request, $id)
+    {
+        $data = $request->all();
+        $data['id'] = $id;
+
+        if (!isset($data['id']) ||
+            !is_numeric($data['id']) ||
+            $data['id'] <= 0) {
+            return $this->setResponse([
+                'code' => 500,
+                'title' => "Leave credit ID is invalid.",
+            ]);
+        }
+
+        return $this->absorb($this->leave_credit_repo->fetchLeaveCredit($data))->json();
+    }
+
+    public function update(Request $request, $id)
+    {
+        $data = $request->all();
+        $data['id'] = $id;
+
+        return $this->absorb($this->leave_credit_repo->defineLeaveCredit($data))->json();
+    }
+
+    public function search(Request $request)
+    {
+        $data = $request->all();
+        return $this->absorb($this->leave_credit_repo->searchLeaveCredit($data))->json();
+    }
+
+}
