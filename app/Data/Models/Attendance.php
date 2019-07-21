fe14b07eb005481fa34e718b3fa93c6a64adce53
Floyd Matabilas <fmatabilas@maersummit.tech>
2019-07-22 00:06:49 +0800

    Floyd > add is_leave fillable field

--- app/Data/Models/Attendance.php
+++ app/Data/Models/Attendance.php
@@ -3,7 +3,6 @@
 namespace App\Data\Models;
 
 use App\Data\Models\BaseModel;
-use Illuminate\Database\Eloquent\Model;
 use Carbon\Carbon;
 
 class Attendance extends BaseModel
@@ -11,32 +10,37 @@ class Attendance extends BaseModel
     protected $fillable = [
         'schedule_id',
         'time_in',
-        'time_out'
+        'time_out',
+        'is_leave',
     ];
 
     protected $appends = [
-        'rendered_time'
+        'rendered_time',
     ];
 
     protected $hidden = ['created_at', 'updated_at', 'deleted_at'];
 
-    public function getTimeInAttribute($value) {
+    public function getTimeInAttribute($value)
+    {
         return $value;
     }
 
-    public function getTimeOutAttribute($value) {
+    public function getTimeOutAttribute($value)
+    {
         return $value;
     }
 
-    public function getRenderedTimeAttribute() {
+    public function getRenderedTimeAttribute()
+    {
         $start = Carbon::parse($this->time_in);
         $end = ($this->time_out) ? Carbon::parse($this->time_out) : Carbon::now();
-        $difference  = $end->diffInSeconds($start);
+        $difference = $end->diffInSeconds($start);
 
         return $difference;
     }
 
-    public function schedule(){
-        return $this->hasOne('App\Data\Models\AgentSchedule', 'id','schedule_id');
+    public function schedule()
+    {
+        return $this->hasOne('App\Data\Models\AgentSchedule', 'id', 'schedule_id');
     }
 }
