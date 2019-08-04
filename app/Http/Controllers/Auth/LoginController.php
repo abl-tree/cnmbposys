198f1a8daed8fc98eaeaf5063f1d8693a3b761ff
Floyd Matabilas <fmatabilas@maersummit.tech>
2019-08-04 17:52:17 +0800

    Floyd > refactor passport login

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -86,7 +86,14 @@ class LoginController extends Controller
         $data['client_secret'] = env("ACCOUNTS_API_CLIENT_SECRET");
         $data['scope'] = '*';
 
-        if (Auth::attempt(['email' => request('username'), 'password' => request('password')])) {
+        if (!isset($data['username']) || !isset($data['password'])) {
+            return response()->json([
+                'code' => 500,
+                'title' => 'Incomplete credetials',
+            ], 500);
+        }
+
+        if (Auth::attempt(['email' => $data['username'], 'password' => $data['password']])) {
             $user = Auth::user();
             $success['access_token'] = $user->createToken('CNM')->accessToken;
             return response()->json(
@@ -102,7 +109,7 @@ class LoginController extends Controller
             return response()->json(
                 [
                     'code' => 401,
-                    "title" => 'Unauthorized access / Invalid credentials',
+                    "title" => 'Unauthorized access | Invalid credentials',
                     'meta' => [
                         'username' => $data['username'],
                     ],
