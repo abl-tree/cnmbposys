2eb8c8c1a1837f37f85e984a97d6f7c5aea681b1
Allen Beciera Lamparas <ablamparas@gmail.com>
2019-09-11 01:50:06 +0800

    tl and om ids params

--- app/Data/Repositories/AgentScheduleRepository.php
+++ app/Data/Repositories/AgentScheduleRepository.php
@@ -1106,6 +1106,38 @@ class AgentScheduleRepository extends BaseRepository
 
                 }
 
+                if(isset($data['tl_id']) && isset($data['om_id'])) {
+                    $result = $result->where(function($q) use ($data) {
+                        $q->whereHas('hierarchy', function($q) use ($data) {
+                            $q->where('parent_id', $data['tl_id']);
+                        });
+                        $q->whereHas('hierarchy', function($q) use ($data) {
+                            $q->where('parent_id', $data['tl_id']);
+                            $q->whereHas('parentInfo', function($q) use ($data) {
+                                $q->whereHas('accesslevelhierarchy', function($q) use ($data) {
+                                    $q->where('parent_id', $data['om_id']);
+                                });
+                            });
+                        });
+                    });
+                } else if(isset($data['tl_id'])) {
+                    $result = $result->where(function($q) use ($data) {
+                        $q->whereHas('hierarchy', function($q) use ($data) {
+                            $q->where('parent_id', $data['tl_id']);
+                        });
+                    });
+                } else if(isset($data['om_id'])) {
+                    $result = $result->where(function($q) use ($data) {
+                        $q->whereHas('hierarchy', function($q) use ($data) {
+                            $q->whereHas('parentInfo', function($q) use ($data) {
+                                $q->whereHas('accesslevelhierarchy', function($q) use ($data) {
+                                    $q->where('parent_id', $data['om_id']);
+                                });
+                            });
+                        });
+                    });
+                }
+
                 $data['relations'] = array('schedule' => function ($query) use ($parameters) {
                     $end = Carbon::parse($parameters['end']);
                     $end = ($end->isToday()) ? Carbon::now() : $end->addDays(1);
