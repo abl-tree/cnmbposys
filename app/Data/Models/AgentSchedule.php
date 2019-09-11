b86088c0cc9f57f20421c12527d8b03b9b180e37
Ximdrake <asidorx@gmail.com>
2019-09-11 13:25:01 +0800

    update

--- app/Data/Models/AgentSchedule.php
+++ app/Data/Models/AgentSchedule.php
@@ -82,6 +82,7 @@ class AgentSchedule extends BaseModel
     {
             $data=null;
             $dates=[];
+            $interval=[];
             foreach ($this->user as $key => $value) {
                 if($value->hired_date!=null){
                     array_push($dates,date("Y-m-d", strtotime($value->hired_date)));
@@ -94,7 +95,7 @@ class AgentSchedule extends BaseModel
                 {
                     if( strtotime($this->date['ymd']) >= strtotime($day) ) {
                     //$interval[$count] = abs(strtotime($date) - strtotime($day));
-                    $interval[] = abs(strtotime( $this->date['ymd']) - strtotime($day));
+                     array_push($interval,abs(strtotime( $this->date['ymd']) - strtotime($day)));
                     //$count++;
                     }
                 }
