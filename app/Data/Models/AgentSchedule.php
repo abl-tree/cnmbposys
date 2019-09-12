28da920bc10920648150913f8f57384475138547
Floyd Matabilas <fmatabilas@maersummit.tech>
2019-09-12 09:54:29 +0800

    Floyd > refactor formatting

--- app/Data/Models/AgentSchedule.php
+++ app/Data/Models/AgentSchedule.php
@@ -86,50 +86,49 @@ class AgentSchedule extends BaseModel
 
     public function getUserstatusAttribute()
     {
-            $data=null;
-            $dates=[];
-            $interval=[];
-            foreach ($this->user as $key => $value) {
-                if($value->hired_date!=null){
-                    array_push($dates,date("Y-m-d", strtotime($value->hired_date)));
-                }else{
-                    array_push($dates,date("Y-m-d", strtotime($value->separation_date)));
-                }
+        $data = null;
+        $dates = [];
+        $interval = [];
+        foreach ($this->user as $key => $value) {
+            if ($value->hired_date != null) {
+                array_push($dates, date("Y-m-d", strtotime($value->hired_date)));
+            } else {
+                array_push($dates, date("Y-m-d", strtotime($value->separation_date)));
             }
-   
-                foreach($dates as $day)
-                {
-                    if( strtotime($this->date['ymd']) >= strtotime($day) ) {
-                    //$interval[$count] = abs(strtotime($date) - strtotime($day));
-                     array_push($interval,abs(strtotime( $this->date['ymd']) - strtotime($day)));
-                    //$count++;
-                    }
-                }
-                if(count($interval) != 0){
-                asort($interval);
-                $closest = key($interval);
-                foreach ($this->user as $key => $value) {
-                    if(date("Y-m-d", strtotime($value->hired_date))==$dates[$closest]){
-                       return array(
+        }
+
+        foreach ($dates as $day) {
+            if (strtotime($this->date['ymd']) >= strtotime($day)) {
+                //$interval[$count] = abs(strtotime($date) - strtotime($day));
+                array_push($interval, abs(strtotime($this->date['ymd']) - strtotime($day)));
+                //$count++;
+            }
+        }
+        if (count($interval) != 0) {
+            asort($interval);
+            $closest = key($interval);
+            foreach ($this->user as $key => $value) {
+                if (date("Y-m-d", strtotime($value->hired_date)) == $dates[$closest]) {
+                    return array(
                         'hired_date' => date("Y-m-d", strtotime($value->hired_date)),
                         'status' => $value->status,
                         'type' => $value->type,
                     );
-                    }else if(date("Y-m-d", strtotime($value->separation_date))==$dates[$closest]){
-                        return array(
-                            'hired_date' => date("Y-m-d", strtotime($value->separation_date)),
-                            'status' => $value->status,
-                            'type' => $value->type,
-                        );
-                    }
+                } else if (date("Y-m-d", strtotime($value->separation_date)) == $dates[$closest]) {
+                    return array(
+                        'hired_date' => date("Y-m-d", strtotime($value->separation_date)),
+                        'status' => $value->status,
+                        'type' => $value->type,
+                    );
                 }
-                }else{
-                    return "Not Yet Hired ";  
-                }
-                
-              //  return $dates[$closest];
-         //  return  $this->date['ymd'];
-       
+            }
+        } else {
+            return "Not Yet Hired ";
+        }
+
+        //  return $dates[$closest];
+        //  return  $this->date['ymd'];
+
     }
 
     public function getConformanceAttribute($value)
