e257febf1904c982db8893e7be58cc1c9c76272e
Emmanuel James <jamesenglajom@gmail.com>
2019-10-31 07:13:43 +0800

    Emman > appended timeout origin system/user and autotimeout after 1hour

--- app/Data/Models/Attendance.php
+++ app/Data/Models/Attendance.php
@@ -3,6 +3,7 @@
 namespace App\Data\Models;
 
 use App\Data\Models\BaseModel;
+use App\Data\Models\Attendance;
 use Carbon\Carbon;
 
 class Attendance extends BaseModel
@@ -17,7 +18,8 @@ class Attendance extends BaseModel
     ];
 
     protected $appends = [
-        'rendered_time',
+        'rendered_time', 
+        'timeout_origin'
     ];
 
     protected $hidden = ['created_at', 'updated_at', 'deleted_at', 'raw_time_out'];
@@ -29,7 +31,25 @@ class Attendance extends BaseModel
 
     public function getTimeOutAttribute($value)
     {
-        return $value ? Carbon::parse($value) : null;
+        // return $value ? Carbon::parse($value) : null;
+        
+        $schedule = $this->with("schedule")->find($this->id)->schedule;
+        $delay = Carbon::parse($schedule->end_event)->addHour();
+        $now = Carbon::now();
+
+        // return 
+        if($this->time_in){
+            if(!$value){
+                if($now->isAfter($delay)){
+                    return Carbon::parse($schedule->end_event);
+                }
+                    return null;
+            }
+            return Carbon::parse($value);
+        }else{
+            return null;
+        }
+
     }
 
     public function getRenderedTimeAttribute()
@@ -39,6 +59,25 @@ class Attendance extends BaseModel
         $difference = $end->diffInSeconds($start);
 
         return $difference;
+    }
+
+    public function getTimeoutOriginAttribute(){
+        $schedule = $this->with("schedule")->find($this->id)->schedule;
+        $delay = Carbon::parse($schedule->end_event)->addHour();
+        $now = Carbon::now();
+
+        // return 
+        if($this->time_in){
+            if(!$this->time_out){
+                if($now->isAfter($delay)){
+                    return "system";
+                }
+                    return null;
+            }
+            return "user";
+        }else{
+            return null;
+        }
     }
 
     public function schedule()
