e6132de7aa76a4e54a29342122a71adb27e6793d
Floyd Matabilas <floydignified@gmail.com>
2020-07-25 21:33:45 +0800

    Floyd > add limit on maximum failed login attempts (5 minutes lockout)

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -2,13 +2,14 @@
 
 namespace App\Http\Controllers\Auth;
 
+use App\Data\Models\UserInfo;
 use App\Http\Controllers\Controller;
 use App\User;
-use App\Data\Models\UserInfo;
+use Carbon\Carbon;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use Carbon\Carbon;
+use Illuminate\Support\Facades\Lang;
 
 class LoginController extends Controller
 {
@@ -31,6 +32,8 @@ class LoginController extends Controller
      * @var string
      */
     protected $redirectTo = '/';
+    protected $maxAttempts = 5;
+    protected $decayMinutes = 5;
 
     /**
      * Create a new controller instance.
@@ -95,16 +98,35 @@ class LoginController extends Controller
             ], 500);
         }
 
+        // max failed login attemps check
+        if ($this->hasTooManyLoginAttempts($request)) {
+            $this->fireLockoutEvent($request);
+
+            $seconds = $this->limiter()->availableIn(
+                $this->throttleKey($request)
+            );
+            return response()->json([
+                'code' => 429,
+                'title' => Lang::get('auth.throttle', ['seconds' => $seconds]),
+                'meta' => [
+                    'username' => $data['username'],
+                ],
+            ], 429);
+        }
+
         if (Auth::attempt(['email' => $data['username'], 'password' => $data['password']])) {
+            // clear lockout count on successful login
+            $this->clearLoginAttempts($request);
+
             $user = Auth::user();
             $status = UserInfo::find($user->uid)->status;
-            $password_updated=Auth::user()->password_updated;
-            $loginflag=Auth::user()->loginflag;
+            $password_updated = Auth::user()->password_updated;
+            $loginflag = Auth::user()->loginflag;
             $date = Carbon::parse($password_updated);
             $now = Carbon::now();
-            $diff = $date->diffInDays($now)+1;
-            if(strtolower($status)=="active"){
-                if($loginflag!==0&&$diff>31){
+            $diff = $date->diffInDays($now) + 1;
+            if (strtolower($status) == "active") {
+                if ($loginflag !== 0 && $diff > 31) {
                     $success['access_token'] = $user->createToken('CNM')->accessToken;
                     if ($user) {
                         $user['loginFlag'] = 0;
@@ -126,25 +148,25 @@ class LoginController extends Controller
                                     'meta' => [
                                         'token' => $success,
                                         'user' => $user,
-                                        'days_difference'=>$diff
+                                        'days_difference' => $diff,
                                     ],
                                 ], 200);
                         }
                     }
-    
-                }else{
-                $success['access_token'] = $user->createToken('CNM')->accessToken;
-                return response()->json(
-                    [
-                        'code' => 200,
-                        "title" => 'Successfully logged in',
-                        'meta' => [
-                            'token' => $success,
-                            'user' => $user,
-                        ],
-                    ], 200);
+
+                } else {
+                    $success['access_token'] = $user->createToken('CNM')->accessToken;
+                    return response()->json(
+                        [
+                            'code' => 200,
+                            "title" => 'Successfully logged in',
+                            'meta' => [
+                                'token' => $success,
+                                'user' => $user,
+                            ],
+                        ], 200);
                 }
-            }else{
+            } else {
                 return response()->json(
                     [
                         'code' => 500,
@@ -155,6 +177,9 @@ class LoginController extends Controller
                     ], 500);
             }
         } else {
+            // increase lockout count on failed login
+            $this->incrementLoginAttempts($request);
+
             return response()->json(
                 [
                     'code' => 401,
